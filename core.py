9e869ba6ee0b2147b9b6896919e18560ce9e74e7
hatsaale <kkwar7988@gmail.com>
2025-05-27 16:18:14 +0530

    Create core.py

---
+++ core.py
@@ -0,0 +1,236 @@
+# Don't Remove Credit Tg - @Tushar0125
+
+import os
+import time
+import datetime
+import aiohttp
+import aiofiles
+import asyncio
+import logging
+import requests
+import tgcrypto
+import subprocess
+import concurrent.futures
+import re
+
+from utils import progress_bar
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from pytube import Playlist  #Youtube Playlist Extractor
+from yt_dlp import YoutubeDL
+import yt_dlp as youtube_dl
+
+def duration(filename):
+    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", filename],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    return float(result.stdout)
+    
+def exec(cmd):
+        process = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        output = process.stdout.decode()
+        print(output)
+        return output
+
+def pull_run(work, cmds):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=work) as executor:
+        print("Waiting for tasks to complete")
+        fut = executor.map(exec,cmds)
+
+async def aio(url,name):
+    k = f'{name}.pdf'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(k, mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+    return k
+
+async def download(url,name):
+    ka = f'{name}.pdf'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(ka, mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+    return ka
+
+def parse_vid_info(info):
+    info = info.strip()
+    info = info.split("\n")
+    new_info = []
+    temp = []
+    for i in info:
+        i = str(i)
+        if "[" not in i and '---' not in i:
+            while "  " in i:
+                i = i.replace("  ", " ")
+            i.strip()
+            i = i.split("|")[0].split(" ",2)
+            try:
+                if "RESOLUTION" not in i[2] and i[2] not in temp and "audio" not in i[2]:
+                    temp.append(i[2])
+                    new_info.append((i[0], i[2]))
+            except:
+                pass
+    return new_info
+
+def vid_info(info):
+    info = info.strip()
+    info = info.split("\n")
+    new_info = dict()
+    temp = []
+    for i in info:
+        i = str(i)
+        if "[" not in i and '---' not in i:
+            while "  " in i:
+                i = i.replace("  ", " ")
+            i.strip()
+            i = i.split("|")[0].split(" ",3)
+            try:
+                if "RESOLUTION" not in i[2] and i[2] not in temp and "audio" not in i[2]:
+                    temp.append(i[2])
+                    new_info.update({f'{i[2]}':f'{i[0]}'})
+            except:
+                pass
+    return new_info
+
+async def run(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    stdout, stderr = await proc.communicate()
+
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if proc.returncode == 1:
+        return False
+    if stdout:
+        return f'[stdout]\n{stdout.decode()}'
+    if stderr:
+        return f'[stderr]\n{stderr.decode()}'
+
+def old_download(url, file_name, chunk_size = 1024 * 10):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    r = requests.get(url, allow_redirects=True, stream=True)
+    with open(file_name, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            if chunk:
+                fd.write(chunk)
+    return file_name
+
+def human_readable_size(size, decimal_places=2):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB', 'PB']:
+        if size < 1024.0 or unit == 'PB':
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f} {unit}"
+
+def time_name():
+    date = datetime.date.today()
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H%M%S")
+    return f"{date} {current_time}.mp4"
+
+def get_playlist_videos(playlist_url):
+    try:
+        playlist = Playlist(playlist_url)
+        playlist_title = playlist.title
+        videos = {}
+        
+        for video in playlist.videos:
+            try:
+                video_title = video.title
+                video_url = video.watch_url
+                videos[video_title] = video_url
+            except Exception as e:
+                logging.error(f"Could not retrieve video details: {e}")
+        
+        return playlist_title, videos
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return None, None
+
+def save_to_file(video_links, channel_name):
+    sanitized_channel_name = re.sub(r'[^\w\s-]', '', channel_name).strip().replace(' ', '_')
+    filename = f"{sanitized_channel_name}.txt"    
+    with open(filename, 'w', encoding='utf-8') as file:
+        for number, (title, url) in video_links.items():
+            if url.startswith("https://"):
+                formatted_url = url
+            elif "shorts" in url:
+                formatted_url = f"https://www.youtube.com{url}"
+            else:
+                formatted_url = f"https://www.youtube.com/watch?v={url}"
+            file.write(f"{number}. {title}: {formatted_url}\n")
+    return filename
+
+# Global variable for failed counter
+failed_counter = 0
+
+async def default_download(url, cmd, name):
+    """Default download handling for general URLs"""
+    try:
+        if os.path.isfile(name):
+            return name
+        elif os.path.isfile(f"{name}.webm"):
+            return f"{name}.webm"
+        name = name.split(".")[0]
+        if os.path.isfile(f"{name}.mkv"):
+            return f"{name}.mkv"
+        elif os.path.isfile(f"{name}.mp4"):
+            return f"{name}.mp4"
+        elif os.path.isfile(f"{name}.mp4.webm"):
+            return f"{name}.mp4.webm"
+        return name
+    except FileNotFoundError as exc:
+        return f"{name}.mp4"
+
+async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
+    reply = await m.reply_text(f"🚀🚀🚀𝗨𝗣𝗟𝗢𝗔𝗗𝗜𝗡𝗚🚀🚀🚀 » `{name}`\n\n🤖𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 ➤ 𝗧𝗨𝗦𝗛𝗔𝗥")
+    time.sleep(1)
+    start_time = time.time()
+    await m.reply_document(ka,caption=cc1)
+    count+=1
+    await reply.delete (True)
+    time.sleep(1)
+    try:
+        os.remove(ka)
+    except:
+        pass
+    time.sleep(3) 
+
+async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
+    subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:12 -vframes 1 "{filename}.jpg"', shell=True)
+    await prog.delete (True)
+    reply = await m.reply_text(f"**🚀🚀🚀𝗨𝗣𝗟𝗢𝗔𝗗𝗜𝗡𝗚🚀🚀🚀** » `{name}`\n\n🤖𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 ➤ 𝗧𝗨𝗦𝗛𝗔𝗥")
+    try:
+        if thumb == "no":
+            thumbnail = f"{filename}.jpg"
+        else:
+            thumbnail = thumb
+    except Exception as e:
+        await m.reply_text(str(e))
+
+    dur = int(duration(filename))
+    start_time = time.time()
+
+    try:
+        await m.reply_video(filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur, progress=progress_bar,progress_args=(reply,start_time))
+    except Exception:
+        await m.reply_document(filename,caption=cc, progress=progress_bar,progress_args=(reply,start_time))
+
+    try:
+        os.remove(filename)
+        os.remove(f"{filename}.jpg")
+    except:
+        pass
+    await reply.delete (True)
