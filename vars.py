7ae371fd16ee97fe8ace359d365f043fe8db3a05
hatsaale <kkwar7988@gmail.com>
2025-05-27 16:15:43 +0530

    Create  vars.py

---
+++ vars.py
@@ -0,0 +1,8 @@
+# Don't Remove Credit Tg - @Tushar0125
+# Ask Doubt on telegram @Tushar0125
+
+from os import environ
+
+API_ID = int(environ.get("28748671")) #Your new API ID from environment
+API_HASH = environ.get("f53ec7c41ce34e6d585674ed9ce6167c") #Your new API Hash from environment  
+BOT_TOKEN = environ.get("7896165148:AAFKmmxHT01cbgqPcuHpbigHnGETVO4vSk4") #Your new Bot Token from environment
